ea7f8f15963f10216cd50f7d02591fcf644e9bf7
Sehoon1207 <102689847+Sehoon1207@users.noreply.github.com>
2022-03-31 21:58:07 +0200

    Delete HelloWorld_output.c
    ERROR

--- HelloWorld_output.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  // Simple Recipe
-  printf("2 Cups: All Purpose Flour\n");                    // \n 줄 띄우기
-  printf("1 Cups: Unsalted Butter\t(Room Temperature)");    // \t 칸 띄우기
-  printf("\n2/3 Cups: Granulated Sugar");
-  
-}
