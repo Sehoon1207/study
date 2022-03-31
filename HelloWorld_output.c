0e5c7d7fe77dc41608fe342d27bd4ab74f8f1ef8
Sehoon <is.sehoon@gmail.com>
2022-03-31 21:37:16 +0200

    2 file upload

---
+++ HelloWorld_output.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  // Simple Recipe
+  printf("2 Cups: All Purpose Flour\n");                    // \n 줄 띄우기
+  printf("1 Cups: Unsalted Butter\t(Room Temperature)");    // \t 칸 띄우기
+  printf("\n2/3 Cups: Granulated Sugar");
+  
+}
