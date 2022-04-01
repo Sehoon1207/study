9ffaac94718fcb9e8c31287a24dc92f1faad6659
Sehoon <is.sehoon@gmail.com>
2022-04-01 18:59:09 +0200

    잔디심기 프로젝트

---
+++ HelloWorld_commend.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+// first comment  
+  printf("Chocolate chip cookies are way better than oatmeal raisin cookies.\n");
+
+/* second comment*/
+  
+  printf("Learning the C programming language is an exciting adventure!\n");
+}
