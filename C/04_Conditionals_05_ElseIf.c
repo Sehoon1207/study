679081ddc2914373dc127379afb7cffedf06b36c
Sehoon <is.sehoon@gmail.com>
2022-04-06 21:01:28 +0200

    1 Day 1 Commit

---
+++ C/04_Conditionals_05_ElseIf.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+  double ph = 7;
+
+  // Start the if, else if, else here:
+if (ph > 7) {
+  printf("Basic");
+} else if (ph < 7) {
+  printf("Acidic");
+} else {
+  printf("Neutral");
+}
+
+
+/*
+if (condition) {
+  // Some code
+} else if (condition) {
+  // Some code
+} else {
+  // Some code
+}
+*/
+
+
+
+}
