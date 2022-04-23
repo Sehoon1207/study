ab521342cac11e437e915f226ede3f81dadffcd0
Sehoon <is.sehoon@gmail.com>
2022-04-23 02:48:57 +0200

    1 Day 1 Commit

---
+++ C/10_Structures_07_Summary.c
@@ -0,0 +1,17 @@
+/*
+Summary
+
+We’ve covered many important topics:
+
+Structures allow you to create user-defined data types
+Structures can package together other data types into one type
+You can define a structure using the struct keyword
+When defining a structure, the member variables are declared but not instantiated
+Structures help programs work with complex data and can represent real-world objects
+You can access member variables with the dot operator
+You can have a pointer to a structure and access its member variables using the arrow operator
+You can pass structures and structure pointers to functions
+You can return structures from functions
+With our understanding of structures, we can now create and work with complicated data types easily and efficiently.
+
+*/
