ecc341b9f0eb807954722b91e085eb9735ec3fd0
Sehoon <is.sehoon@gmail.com>
2022-04-29 03:57:17 +0200

    1 Day 1 Commit

---
+++ Python2/01_HelloWorld_10_MultiLineStrings.py
@@ -0,0 +1,9 @@
+haiku = """
+The old pond,
+A frog jumps in:
+Plop!
+"""
+
+"""
+When a string like this is not assigned to a variable, it works as a multi-line comment. This can be helpful as your code gets more complex:
+"""
