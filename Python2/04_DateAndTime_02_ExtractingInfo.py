b4204e8b2ec7854ef745f167f7f0b06087881f03
Sehoon <is.sehoon@gmail.com>
2022-05-11 15:05:03 +0200

    1 Day 1 Commit

---
+++ Python2/04_DateAndTime_02_ExtractingInfo.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+now = datetime.now()
+print (now)
+
+# current_year = now.year
+# current_month = now.month
+current_day = now.day
+
+print (now.month)
+print (current_day)
