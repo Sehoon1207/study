e89d75ece201ff6bef50c4852a932d451a1993be
Sehoon <is.sehoon@gmail.com>
2022-05-18 23:35:46 +0200

    1 Day 1 Commit

--- Python2/06_Functions_03_ParametersAndArguments.py
+++ Python2/06_Functions_03_ParametersAndArguments.py
@@ -3,3 +3,13 @@ def power(base, exponent):  # Add your parameters here!
   print ("%d to the power of %d is %d." % (base, exponent, result))
 
 power(37, 4)  # Add your arguments here!
+
+
+# py square(10)
+
+# Here, the function square was called with the parameter n set to the argument 10.
+
+# To summarize:
+
+# When defining a function, placeholder variables are called parameters.
+# When using, or calling, a function, inputs into the function are called arguments.
