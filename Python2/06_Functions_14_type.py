f90371546fdd94c65df62f25b092ed376892058b
Sehoon <is.sehoon@gmail.com>
2022-05-23 15:51:14 +0200

    1 Day 1 Commit

---
+++ Python2/06_Functions_14_type.py
@@ -0,0 +1,7 @@
+# Print out the types of an integer, a float,
+# and a string on separate lines below.
+
+print (type(42))
+print (type(4.2))
+print (type('spam'))
+print (type('I have to push the pram a lot'))
